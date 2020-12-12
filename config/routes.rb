5c92875d9625aaf1ee772304e92875735326942d
Alejaldo <statfluct@yahoo.com>
2020-12-12 20:53:06 +0500

    Adding of comments output on show.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,12 @@
 Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'terms/index'
-  get 'about/index'
 
   get 'terms' => 'pages#terms'
   get 'about' => 'pages#about'
 
   resource :contacts, only: [:new, :create], path_names: { :new => '' } 
   resources :articles do
-    resources :comments
+    resources :comments, only: [:create] 
   end
 end
