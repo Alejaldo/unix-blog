afa5ab5800c4c92113ffa96fccb5978b7e09c62f
Alejaldo <statfluct@yahoo.com>
2020-12-15 12:21:42 +0500

    Update-zaebate

--- db/migrate/20201215071254_add_username_index.rb
+++ db/migrate/20201215071254_add_username_index.rb
@@ -1,5 +1,5 @@
 class AddUsernameIndex < ActiveRecord::Migration[6.0]
   def change
-    add_index :users, :email, unique: true #like at ...devisw_create_users.rb
+    add_index :users, :username, unique: true #like at ...devisw_create_users.rb
   end
 end
