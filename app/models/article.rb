afbf418fedd8ecf15f36d1877a56fab763a8efe3
Alejaldo <statfluct@yahoo.com>
2020-12-21 11:21:34 +0500

    Add optional: true for belongs_to :user in Article model

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,9 +3,13 @@ class Article < ApplicationRecord
   validates :text, presence: true
 
   has_many :comments
-  belongs_to :user
+  belongs_to :user, optional: true
 
   def subject
     title
+  end
+
+  def last_comment
+    comments.last
   end
 end
