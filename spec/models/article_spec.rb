c76b95e803f3005a604c75e91f595ec1649fdc7f
Alejaldo <statfluct@yahoo.com>
2020-12-21 10:51:11 +0500

    Add factory_bot, subject method for article

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -9,4 +9,11 @@ describe Article do
   describe "associations" do
     it { should have_many :comments }
   end
+
+  describe "#subject" do
+    it "returns the article subject" do
+      article = create(:article, title: "Lorem Ipsum") #create article object in tricky way
+      expect(article.subject).to eq 'Lorem Ipsum' #assert
+    end
+  end
 end
