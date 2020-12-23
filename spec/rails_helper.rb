f34c038e416c1af801b50f45a630a6a23466116b
Alejaldo <statfluct@yahoo.com>
2020-12-23 12:08:13 +0500

    Refactoring: move sign_up to rails_helper

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -73,3 +73,14 @@ end
 RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
 end
+
+def sign_up
+  visit new_user_registration_path
+
+  fill_in :user_email, :with => 'user@example.com'
+  fill_in :user_username, :with => 'miho'
+  fill_in :user_password, :with => '12345678'
+  fill_in :user_password_confirmation, :with => '12345678'
+
+  click_button 'Sign up'
+end
